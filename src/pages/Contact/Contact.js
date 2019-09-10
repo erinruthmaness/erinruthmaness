072520c296a4912242e9f76608ed53275dcc6ce4
Erin Ruth Maness <erinruthmaness@yahoo.com>
2019-09-10 12:07:08 -0400

    updated contact info and redirect page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,18 +4,20 @@ import "./Contact.css";
 
 function Contact() {
     return (
-        <div id="contact-top">
-            <img alt="Erin Ruth Maness" src={photo} id="contact-main-photo" />
-            <div id="contact-name">
-                <h1>Erin Ruth Maness</h1>
-                <h3>Charlotte, North Carolina</h3>
-            </div>
+        <div id="contact-page">
+            <div id="contact-top">
+                <img alt="Erin Ruth Maness" src={photo} id="contact-main-photo" />
+                <div id="contact-name">
+                    <h1>Erin Ruth Maness</h1>
+                    <h3>Charlotte, North Carolina</h3>
+                </div>
+            </div >
             <div id="contact-body">
-                <h3>LinkedIn: <a 
-                href="https://www.linkedin.com/in/erin-maness-619286127/">Erin Ruth Maness</a></h3>
-                <h3>Email: erinruthmaness@gmail.com</h3>
+                <h2>LinkedIn: <a
+                    href="https://www.linkedin.com/in/erin-maness-619286127/">Erin Ruth Maness</a></h2>
+                <h2>Email: erinruthmaness@gmail.com</h2>
             </div>
-        </div >
+        </div>
 
     )
 }
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from "react";
+import photo from "../../assets/erinruthmaness.jpg";
 import "./NotFound.css";
 
 function NotFound() {
     return (
-        <div  className="page-body">
-            Nothing here!
+        <div className="page-body">
+            <div id="wrong-page">
+                <img alt="Erin Ruth Maness" src={photo} id="wrong-photo" />
+                <h1 id="nope">There's nothing here!</h1>
+            </div>
         </div>
     )
 }
